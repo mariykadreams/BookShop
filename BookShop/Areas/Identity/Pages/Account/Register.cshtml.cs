71137641caa384a7d802f247a065371210e2349e
mariia tokhtamysh <mariyka.tokhtamysh@gmail.com>
2024-06-29 19:58:06 +0300

    register logic update

--- BookShop/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BookShop/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -149,6 +149,8 @@ namespace BookShop.Areas.Identity.Pages.Account
                 {
                     _logger.LogInformation("User created a new account with password.");
 
+                    if()
+
                     var userId = await _userManager.GetUserIdAsync(user);
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                     code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
